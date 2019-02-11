77bd7d69344412436af7abd797acccf1331cb48b
marvin evins <maevins@cps.edu>
2019-02-10 23:29:42 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+/**
+ * Navbar
+ */
+export class Navbar extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>MY COMPONENT</div>
+    );
+  }
+}
+
+
+export default Navbar;
---
+++ src/components/ProductList.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+
+/**
+ * ProductList
+ */
+export class ProductList extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>MY COMPONENT</div>
+    );
+  }
+}
+
+
+
+export default ProductList;
