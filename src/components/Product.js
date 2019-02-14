d2ae0468c6f039fdae1f1afacf9384965462fb9b
marvin evins <maevins@cps.edu>
2019-02-14 05:04:08 -0600

    handling the small image error not solved yet

--- src/components/Product.js
+++ src/components/Product.js
@@ -11,7 +11,7 @@ export default class Product extends Component { // eslint-disable-line react/pr
     const{id, title, img, price, inCart} = this.props.product;
     return (
       <div>
-      <ProductWrapper  className="col-9 mx-auto col-md-6 col-lg-3">
+      <ProductWrapper  className="col-9 mx-auto col-md-6 col-lg-3 my-3">
 
         <div className="card">
           <div className="img-container p-5"
